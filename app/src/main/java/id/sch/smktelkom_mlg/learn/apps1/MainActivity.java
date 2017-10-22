bab1b1ab95aa4a2ab1ee2a1afd4b5c2fdb309630
dhenarraisyahs <dhenarra18@gmail.com>
2017-10-22 23:19:48 +0700

    Street View Panorama

--- app/src/main/java/id/sch/smktelkom_mlg/learn/apps1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/apps1/MainActivity.java
@@ -1,18 +1,17 @@
 package id.sch.smktelkom_mlg.learn.apps1;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
     static final CameraPosition kantor = CameraPosition.builder()
             .target(new LatLng(-6.182419, 106.830236))
             .zoom(15)
@@ -28,21 +27,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        StreetViewPanoramaFragment svpFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.map);
+        svpFragment.getStreetViewPanoramaAsync(this);
     }
 
 
+
     @Override
-    public void onMapReady(GoogleMap map) {
-        mapReady = true;
-        googleMap = map;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(kantor));
-        map.addCircle(new CircleOptions()
-                .center(KANTOR)
-                .radius(125)
-                .strokeColor(Color.GRAY)
-                .fillColor(Color.argb(64, 0, 255, 0)));
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewPanorama.setPosition(KANTOR);
+        StreetViewPanoramaCamera camera = StreetViewPanoramaCamera.builder()
+                .bearing(180)
+                .build();
+        streetViewPanorama.animateTo(camera, 1000);
     }
 }
