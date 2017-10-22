20f1d36f6c6fac89bdd9f74794a13ff4686e26b5
dhenarraisyahs <dhenarra18@gmail.com>
2017-10-22 16:44:58 +0700

    Moving Camera

--- app/src/main/java/id/sch/smktelkom_mlg/learn/apps1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/apps1/MainActivity.java
@@ -13,39 +13,64 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    Button maps, satelite, hybrid;
+    Button telkom, mulmed, palace;
     GoogleMap googleMap;
     boolean mapReady = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        maps = (Button) findViewById(R.id.bMap);
-        maps.setOnClickListener(new View.OnClickListener() {
+        telkom = (Button) findViewById(R.id.bTelkom);
+        telkom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mapReady)
-                    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                if (mapReady) {
+                    LatLng telkom = new LatLng(-7.976830, 112.658958);
+                    CameraPosition cp = CameraPosition.builder()
+                            .target(telkom)
+                            .zoom(14)
+                            .bearing(295)
+                            .tilt(75)
+                            .build();
+                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 10000, null);
+                }
             }
         });
 
-        satelite = (Button) findViewById(R.id.bSatelite);
-        satelite.setOnClickListener(new View.OnClickListener() {
+        mulmed = (Button) findViewById(R.id.bMulmed);
+        mulmed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mapReady)
-                    googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                if (mapReady) {
+                    LatLng mulmed = new LatLng(-6.182464, 106.830203);
+                    CameraPosition cp = CameraPosition.builder()
+                            .target(mulmed)
+                            .zoom(14)
+                            .bearing(295)
+                            .tilt(75)
+                            .build();
+                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 10000, null);
+                }
             }
         });
 
-        hybrid = (Button) findViewById(R.id.bHybrid);
-        hybrid.setOnClickListener(new View.OnClickListener() {
+        palace = (Button) findViewById(R.id.bPalace);
+        palace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mapReady)
-                    googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                if (mapReady) {
+                    LatLng palace = new LatLng(51.501364, -0.141890);
+                    CameraPosition cp = CameraPosition.builder()
+                            .target(palace)
+                            .zoom(14)
+                            .bearing(295)
+                            .tilt(75)
+                            .build();
+                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 10000, null);
+                }
             }
         });
 
@@ -53,12 +78,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+
     @Override
     public void onMapReady(GoogleMap map) {
         mapReady = true;
         googleMap = map;
-        LatLng telkom = new LatLng(-7.976830, 112.658958);
-        CameraPosition position = CameraPosition.builder().target(telkom).zoom(14).build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(position));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
