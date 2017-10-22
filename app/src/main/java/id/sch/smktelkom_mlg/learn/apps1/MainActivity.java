9f773df1077683f174ab5a1d15862ffce087a95e
dhenarraisyahs <dhenarra18@gmail.com>
2017-10-22 20:46:49 +0700

    Marker

--- app/src/main/java/id/sch/smktelkom_mlg/learn/apps1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/apps1/MainActivity.java
@@ -2,8 +2,6 @@ package id.sch.smktelkom_mlg.learn.apps1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,69 +9,38 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    Button telkom, mulmed, palace;
+    static final CameraPosition Jakarta = CameraPosition.builder()
+            .target(new LatLng(-6.186486, 106.834091))
+            .zoom(15)
+            .tilt(45)
+            .build();
     GoogleMap googleMap;
     boolean mapReady = false;
-
+    MarkerOptions plazaIndonesia, grandIndonesia, atriumPlaza, tamanAnggrek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        telkom = (Button) findViewById(R.id.bTelkom);
-        telkom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mapReady) {
-                    LatLng telkom = new LatLng(-7.976830, 112.658958);
-                    CameraPosition cp = CameraPosition.builder()
-                            .target(telkom)
-                            .zoom(14)
-                            .bearing(295)
-                            .tilt(75)
-                            .build();
-                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 10000, null);
-                }
-            }
-        });
+        plazaIndonesia = new MarkerOptions()
+                .position(new LatLng(-6.193060, 106.821949))
+                .title("Plaza Indonesia");
 
-        mulmed = (Button) findViewById(R.id.bMulmed);
-        mulmed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mapReady) {
-                    LatLng mulmed = new LatLng(-6.182464, 106.830203);
-                    CameraPosition cp = CameraPosition.builder()
-                            .target(mulmed)
-                            .zoom(14)
-                            .bearing(295)
-                            .tilt(75)
-                            .build();
-                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 10000, null);
-                }
-            }
-        });
+        grandIndonesia = new MarkerOptions()
+                .position(new LatLng(-6.195150, 106.819720))
+                .title("Grand Indonesia");
 
-        palace = (Button) findViewById(R.id.bPalace);
-        palace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mapReady) {
-                    LatLng palace = new LatLng(51.501364, -0.141890);
-                    CameraPosition cp = CameraPosition.builder()
-                            .target(palace)
-                            .zoom(14)
-                            .bearing(295)
-                            .tilt(75)
-                            .build();
-                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 10000, null);
-                }
-            }
-        });
+        atriumPlaza = new MarkerOptions()
+                .position(new LatLng(-6.176898, 106.841367))
+                .title("Plaza Atrium");
 
+        tamanAnggrek = new MarkerOptions()
+                .position(new LatLng(-6.178768, 106.792098))
+                .title("Taman Anggrek Mall");
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -83,6 +50,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         mapReady = true;
         googleMap = map;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.addMarker(grandIndonesia);
+        map.addMarker(plazaIndonesia);
+        map.addMarker(tamanAnggrek);
+        map.addMarker(atriumPlaza);
+        flyTo(Jakarta);
+    }
+
+    private void flyTo(CameraPosition target) {
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 }
